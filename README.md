d0257284709b2f62f1095ecab6e35d0f30113e8e
Grant Klinsing <klingra@chrobinson.com>
2023-03-13 14:56:04 -0500

    esm version

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The module used by `myUI` to demonstrate JS module development
 
-# Example/Branch: npm-pack
+# Example/Branch: esm
--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function capitalize(word){
-  console.info('==> myModule: npm-pack');
+export function capitalize(word){
+  console.info('==> myModule: esm module');
   return word.charAt(0).toUpperCase() + word.substr(1, word.length - 1).toLowerCase();
-}
-
-module.exports = {
-  capitalize: capitalize
 }
